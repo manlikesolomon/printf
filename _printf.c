f8e29c2016fae72df26a36b131b664d775eb7384
Gloryc0de <glorysampson2024@gmail.com>
2022-11-22 22:27:24 +0000

    updated printf

--- _printf.c
+++ _printf.c
@@ -34,3 +34,39 @@ int charsFormats(const char *format, va_list args)
 		{"%", _percent}, {"d", _integer}, {"i", _integer}, {NULL, NULL}
 	}
 	chars = 0;
+	for (a = 0; format[a] != '\0'; a++)
+	{
+		if (format[a] == '%')
+		{
+			for (b = 0; f_list[b].sym != NULL; b++)
+			{
+				if (format[a + 1] == f_list[b].sym[0])
+				{
+					r_val = f_list[b].f(args);
+					if (r_val == -1)
+						return (-1);
+					chars += r_val;
+					break;
+				}
+			}
+			if (f_list[b].sym == NULL && format[a + 1] != ' ')
+			{
+				if (format[a + 1] != '\0')
+				{
+					_putchar(format[a]);
+					_putchar(format[a + 1]);
+					chars = chars + 2;
+				}
+				else
+					return (-1);
+			}
+		a += 1;
+		}
+		else
+		{
+			_putchar(format[a]);
+			chars++;
+		}
+	}
+	return (chars);
+}
