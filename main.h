b686cda831caa10e78794ba197de1d4da03ab40e
Gloryc0de <glorysampson2024@gmail.com>
2022-11-22 21:44:20 +0000

    edited header file

--- main.h
+++ main.h
@@ -4,20 +4,18 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+typedef struct formats
+{
+	char *sym;
+	int (*f)(va_list);
+} fmtsSpefier;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int _putchar(char c);
+int _printf(const char *format, ...);
+int charsFormats(const char *format, va_list args);
+int _percent(va_list list);
+int _char(va_list list);
+int _string(va_list list);
+int _integer(va_list list);
 
 #endif
